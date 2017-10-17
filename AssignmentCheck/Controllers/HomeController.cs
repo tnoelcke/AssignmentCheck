767a06a38f3ae3b3299a1f342b142154038d8f27
Mark Clements <mark.clements@oregonstate.edu>
2017-10-17 00:34:34 -0700

    Added admin page that can view all folder status.

--- AssignmentCheck/Controllers/HomeController.cs
+++ AssignmentCheck/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using AssignmentCheck.Extensions;
 using AssignmentCheck.Models;
+using System.IO;
 
 namespace AssignmentCheck.Controllers
 {
@@ -30,7 +31,34 @@ namespace AssignmentCheck.Controllers
                 UserName = userName,
                 Results = assignments.Select(a => a.Validate(path))
             };
-            
+
+            return View(model);
+        }
+
+        public ActionResult Admin(int id)
+        {
+            //gets the root path to the class folder structure
+            string path = AssignmentCheck.Properties.Settings.Default.RootPath;
+            //gets the configured assignment criteria that do the validation of the folders.
+            List<Assignment> assignments = AssignmentCheck.Properties.Settings.Default.Assignments;
+            Assignment target = null;
+            //get the assignment based on the id
+            if (id < 1 || id > assignments.Count)
+            {
+                return new HttpNotFoundResult();
+            }
+            else
+            {
+                target = assignments[id - 1];
+            }
+            string[] paths = Directory.GetDirectories(path);
+
+            //turn the settings into results in the model
+            AdminViewModel model = new AdminViewModel()
+            {
+                Results = paths.Select(p => new Tuple<string, AssignmentResult>(p, target.Validate(p)))
+            };
+
             return View(model);
         }
     }
